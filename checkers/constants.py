76af99ac7792f9841fc8d7aece4281984249ba37
yura4350 <yura435012@gmail.com>
2023-07-17 17:25:48 +0300

    Added orange color to check work with Github

--- checkers/constants.py
+++ checkers/constants.py
@@ -14,6 +14,7 @@ OCHRE = (204, 119, 34)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 GREY = (128, 128, 128)
+ORANGE = (255, 102, 0)
 
 CROWN = pygame.transform.scale(pygame.image.load('checkers/assets/crown.png'), (44, 25))
 
